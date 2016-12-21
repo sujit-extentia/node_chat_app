c1b6cf185102a5c4c964ad1040e4293ee092565e
Sujit Singh <sujit.extentia@gmail.com>
2016-12-21 15:48:51 +0530

    added code to notify when user joins/leaves the chat, also when he types into the chat

--- index.js
+++ index.js
@@ -7,22 +7,34 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  //console.log('a user connected');
   
   
   socket.on('disconnect', function(){
     console.log('user disconnected');
+	//io.emit('chat_message');
   });
   
    socket.on('send_message', function(msg){
-    console.log('message$$person: ' + msg);
+    //console.log('send_message: ' + msg);
 	io.emit('chat_message', msg);
   });
   
   socket.on('logged_in', function(msg){
-    console.log('message: ' + msg);
+   // console.log('logged_in message: ' + msg);
 	io.emit('show_loggedin', msg);
   });
+  
+  socket.on('notify_typing_event', function(msg){
+    //console.log('notify_typing_event message: ' + msg);
+	io.emit('is_typing', msg);
+  });
+  
+  socket.on('person_disconnected', function(msg){
+    //console.log('notify_typing_event message: ' + msg);
+	io.emit('person_disconnected_ack', msg);
+  });
+  
 });
 
 /*http.listen(3000, function(){
